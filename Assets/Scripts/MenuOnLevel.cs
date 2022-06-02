6875cf4f8d667a43c129ded574d35453e48c0209
Alrandis <79513543+Alrandis@users.noreply.github.com>
2022-06-03 01:04:24 +0700

    Создал скрипт для смены камер и выхода в меню

---
+++ Assets/Scripts/MenuOnLevel.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOnLevel : MonoBehaviour
+{
+    private Camera mainCamera;
+    [SerializeField] private Camera additionalCamera;
+  
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+        mainCamera = Camera.main;
+        additionalCamera.enabled = false;
+    }
+
+    public void ExitMenuPressed()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void SwitchCamersPressed()
+    {
+        if (mainCamera.enabled == true) {
+            mainCamera.enabled = false;
+            additionalCamera.enabled = true;
+        }
+        else
+        {
+            mainCamera.enabled = true;
+            additionalCamera.enabled = false;
+        }
+    }
+}
